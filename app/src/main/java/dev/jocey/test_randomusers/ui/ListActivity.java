47195fa29f7897c0f4174bd3c29cc208671c2101
Oleksandr Holynkov <golinkovsasha@gmail.com>
2021-11-19 14:45:50 +0200

    Init commit

---
+++ app/src/main/java/dev/jocey/test_randomusers/ui/ListActivity.java
@@ -0,0 +1,16 @@
+package dev.jocey.test_randomusers.ui;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import dev.jocey.test_randomusers.R;
+
+public class ListActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
---
+++ app/src/main/java/dev/jocey/test_randomusers/ui/PersonActivity.java
@@ -0,0 +1,6 @@
+package dev.jocey.test_randomusers.ui;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class PersonActivity extends AppCompatActivity {
+}
