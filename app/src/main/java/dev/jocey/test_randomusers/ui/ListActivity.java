d9953d96dc34dc1481c094d00eba487bf16447d8
Oleksandr Holynkov <golinkovsasha@gmail.com>
2021-11-21 22:48:40 +0200

    TEST-4 added entities(first start)
    Signed-off-by: Oleksandr Holynkov <golinkovsasha@gmail.com>

--- app/src/main/java/dev/jocey/test_randomusers/ui/ListActivity.java
+++ app/src/main/java/dev/jocey/test_randomusers/ui/ListActivity.java
@@ -3,8 +3,14 @@ package dev.jocey.test_randomusers.ui;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.TextView;
 
 import dev.jocey.test_randomusers.R;
+import dev.jocey.test_randomusers.model.Fetcher;
+import io.reactivex.rxjava3.android.schedulers.AndroidSchedulers;
+import io.reactivex.rxjava3.disposables.Disposable;
+import io.reactivex.rxjava3.schedulers.Schedulers;
 
 public class ListActivity extends AppCompatActivity {
 
@@ -12,5 +18,7 @@ public class ListActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_list);
+
+
     }
 }
